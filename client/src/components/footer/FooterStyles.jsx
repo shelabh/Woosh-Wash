b128a141dc5ef7924fcbbed5730c58f7500080e3
Shelabh <68127067+shelabh@users.noreply.github.com>
2022-12-03 22:55:14 +0530

    created client

--- client/src/components/footer/FooterStyles.jsx
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Divide = styled.a`
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 2.5rem; 
-`;
--- server/prisma/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client = require("@prisma/client");
-exports.database = new client.PrismaClient();
